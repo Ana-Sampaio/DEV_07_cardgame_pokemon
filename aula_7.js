446188bbdbc9d193d99973d5b0676fd83b9b22a9
Ana Sampaio <analu.sampaio.2812@gmail.com>
2022-04-13 11:37:38 -0300

    criando jogo de cartas (em desenvolvimento)

---
+++ aula_7.js
@@ -0,0 +1,474 @@
+/*Conteúdo da aula*/
+
+var carta1 = {
+	nome:"Bulbasaur",
+	imagem:"https://www.wepc.com/wp-content/uploads/2022/01/bulbasaur-community-day-1200x675.jpg",
+	atributos:{
+		attack: 3,
+		defense: 3,
+		speed: 3
+	}
+};
+var carta2 = {
+	nome:"Charmander",
+	imagem:"https://pm1.narvii.com/6113/447a09dc61f94ad5ca8bb9928004cac6df8a2961_hq.jpg",
+	atributos:{
+		attack: 4,
+		defense: 3,
+		speed: 4
+	}
+};
+var carta3 = {
+	nome:"Squirtle",
+	imagem: "https://pbs.twimg.com/tweet_video_thumb/EZrH3YFXQAIJLcE.jpg",
+	atributos:{
+		attack: 3,
+		defense: 4,
+		speed: 3
+	}
+};
+var carta4 = {
+	nome: "Farfetch’d",
+	imagem: "https://media.redadn.es/imagenes/animanga-5_328179.jpg",
+	atributos:{
+		attack: 6,
+		defense: 4,
+		speed: 4
+	}
+};
+var carta5 = {
+	nome: "Ivysaur",
+	imagem: "http://3.bp.blogspot.com/-NPGl8a7xqIs/UGTuFvRY0NI/AAAAAAAAc_U/CvnFgSQF010/s1600/EH15_Ivysaur_de_Crystal_usando_Hoja_afilada.jpg",
+	atributos:{
+		attack: 4,
+		defense: 4,
+		speed: 4
+	}
+};
+var carta6 = {
+	nome: "Charmeleon",
+	imagem: "https://pm1.narvii.com/6201/00bbe7327767069f8bc711aa9a9307ac6f871329_hq.jpg",
+	atributos:{
+		attack: 4,
+		defense: 4,
+		speed: 5
+	}
+};
+var carta7 = {
+	nome: "Wartortle",
+	imagem: "https://i.ytimg.com/vi/Gj0VeH0MWWM/hqdefault.jpg",
+	atributos:{
+		attack: 4,
+		defense: 5,
+		speed: 5
+	}
+};
+var carta8 = {
+	nome:"Pikachu",
+	imagem: "https://uploads.jovemnerd.com.br/wp-content/uploads/2017/09/pikachu-de-bone-ash-pokemon-sun-moon-filme.jpg",
+	atributos:{
+		attack: 4,
+		defense: 3,
+		speed: 6
+	}
+};
+var carta9 = {
+	nome: "Venusaur",
+	imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYY7lx5G4Zs48UMBM5q4iJ1hpgelJozystBVHf5nBuWxtoefTMT26mON45RjHCGAJObF8&usqp=CAU",
+	atributos:{
+		attack: 5,
+		defense: 5,
+		speed: 5
+	}
+};
+var carta10 = {
+	nome: "Charizard",
+	imagem: "https://heroisx.files.wordpress.com/2009/03/ash-e-charizard.jpg",
+	atributos:{
+		attack: 5,
+		defense: 5,
+		speed: 6
+	}
+};
+var carta11 = {
+	nome: "Blastoise",
+	imagem: "http://4.bp.blogspot.com/-OpOKRY2uK5U/VCWgwCJ8h2I/AAAAAAAABTA/me_hvmoL99M/s1600/pokemon-origin-6.jpg",
+	atributos:{
+		attack: 5,
+		defense: 6,
+		speed: 5
+	}
+};
+var carta12 = {
+	nome: "Raichu",
+	imagem: "https://assets.pokemon.com/assets/cms2/img/watch-pokemon-tv/seasons/season18/season18_ep17_ss04.jpg",
+	atributos:{
+		attack: 6,
+		defense: 4,
+		speed: 7
+	}
+};
+var carta13 = {
+	nome: "Caterpie",
+	imagem: "https://cdn-us.anidb.net/images/main/57653.jpg",
+	atributos:{
+		attack: 2,
+		defense: 3,
+		speed: 3
+	}
+};
+var carta14 = {
+	nome: "Metapod",
+	imagem: "https://vignette.wikia.nocookie.net/poohadventures/images/1/12/800px-Metapod_anime-1-.png/revision/latest?cb=20130826025313",
+	atributos:{
+		attack: 2,
+		defense: 4,
+		speed: 2
+	}
+};
+var carta15 = {
+	nome: "Butterfree",
+	imagem: "https://theclick.gg/wp-content/uploads/2021/09/Butterfree-in-the-Pokemon-Anime.jpg",
+	atributos:{
+		attack: 3,
+		defense: 3,
+		speed: 5
+	}
+};
+var carta16 = {
+	nome: "Ratata",
+	imagem: "https://static.fandomspot.com/images/08/1912/00-featured-rattata-anime-pokemon.jpg",
+	atributos:{
+		attack: 4,
+		defense: 3,
+		speed: 5
+	}
+};
+var carta17= {
+	nome: "Raticate",
+	imagem: "https://theclick.gg/wp-content/uploads/2022/01/Pokemon-Anime-Raticate.jpg",
+	atributos:{
+		attack: 5,
+		defense: 4,
+		speed: 6
+	}
+};
+var carta18= {
+	nome: "Weedle",
+	imagem: "https://2.bp.blogspot.com/-mFpAG7nvR_o/W_hX7XmQ4yI/AAAAAAAAD04/U5bLwWw8CxUqs8HUOOvDAe9GZKnDqzwIACLcBGAs/s1600/weedle.jpg",
+	atributos:{
+		attack: 3,
+		defense: 2,
+		speed: 3
+	}
+};
+var carta19= {
+	nome: "Kakuna",
+	imagem: "https://theclick.gg/wp-content/uploads/2021/12/Pokemon-Anime-Kakuna.jpg",
+	atributos:{
+		attack: 2,
+		defense: 3,
+		speed: 3
+	}
+};
+var carta20= {
+	nome: "Beedrill",
+	imagem: "https://images2.minutemediacdn.com/image/upload/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/dataimagepngbase64iVBORw0KGgoAAAANSUhEUgAABh4AAANx-774ead00d99ca4aac441da5491a9deda.jpg",
+	atributos:{
+		attack: 6,
+		defense: 3,
+		speed: 5
+	}
+};
+var carta21= {
+	nome: "Pidgey",
+	imagem: "https://i.pinimg.com/originals/9d/2e/1c/9d2e1c19b84cb25ddc743d63db6bae7e.jpg",
+	atributos:{
+		attack: 3,
+		defense: 3,
+		speed: 4 
+	}
+};
+var carta22= {
+	nome: "Pidgeotto",
+	imagem: "https://static.fandomspot.com/images/11/10211/00-featured-pidgeotto-flying-in-pokemon-anime.jpg",
+	atributos:{
+		attack: 4,
+		defense: 4,
+		speed: 5
+	}
+};
+var carta23= {
+	nome: "Pidgeot",
+	imagem: "https://theclick.gg/wp-content/uploads/2021/02/pidgeot-pokemon.jpg",
+	atributos:{
+		attack: 5,
+		defense: 5,
+		speed: 6
+	}
+};
+var carta24= {
+	nome: "Spearow",
+	imagem: "https://areajugones.sport.es/wp-content/uploads/2020/09/Spearow-anime-poke%CC%81mon.jpg",
+	atributos:{
+		attack: 4,
+		defense: 2,
+		speed: 5
+	}
+};
+var carta25= {
+	nome: "Fearow",
+	imagem: "https://pbs.twimg.com/tweet_video_thumb/EWEJm5rXQAMJp-J.jpg",
+	atributos:{
+		attack: 6,
+		defense: 4,
+		speed: 6
+	}
+};
+var carta26= {
+	nome: "Ekans",
+	imagem: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/pokemon-anime-ekans-feature.jpg?q=50&fit=contain&w=960&h=480&dpr=1.5",
+	atributos:{
+		attack: 4,
+		defense: 3,
+		speed: 4
+	}
+};
+var carta27= {
+	nome: "Arbok",
+	imagem: "https://static.wikia.nocookie.net/pokemon/images/6/69/Arbok.jpg/revision/latest/scale-to-width-down/250?cb=20130109021954",
+	atributos:{
+		attack: 6,
+		defense: 5,
+		speed: 5
+	}
+};
+var carta28= {
+	nome: "Sandshrew",
+	imagem: "https://www.gamespot.com/a/uploads/screen_kubrick/3/37852/3946772-sandshrew.jpg",
+	atributos:{
+		attack: 5,
+		defense: 5,
+		speed: 3
+	}
+};
+var carta29= {
+	nome: "Sandslash",
+	imagem: "https://pm1.narvii.com/6525/51cd44bc23becfb57cc5dc3a3b1f5e67729549de_hq.jpg",
+	atributos:{
+		attack: 6,
+		defense: 7,
+		speed: 4
+	}
+};
+var carta30= {
+	nome: "Nidoran ♀",
+	imagem: "https://static.wikia.nocookie.net/pokemon/images/4/45/TB102.jpg/revision/latest?cb=20200727222251",
+	atributos:{
+		attack: 3,
+		defense: 4,
+		speed: 3
+	}
+};
+var carta31= {
+	nome: "Nidorina",
+	imagem: "https://media.pocketmonsters.net/characters/1705.png/t/400.jpg",
+	atributos:{
+		attack: 4,
+		defense: 4,
+		speed: 4
+	}
+};
+var carta32= {
+	nome: "Nidoqueen",
+	imagem: "https://pm1.narvii.com/6244/0d51ecd7e46b9daf5f3bb4f6412451668686a773_hq.jpg",
+	atributos:{
+		attack: 6,
+		defense: 6,
+		speed: 5
+	}
+};
+var carta33= {
+	nome: "Nidoran ♂ ",
+	imagem: "https://cdn.anime-planet.com/characters/primary/male-nidoran-1.jpg?t=1625908385",
+	atributos:{
+		attack: 4,
+		defense: 3,
+		speed: 3
+	}
+};
+var carta34= {
+	nome: "Nidorino",
+	imagem: "https://poketouch.files.wordpress.com/2020/05/charging_nidorino_pokemon_the_series.jpg?w=499",
+	atributos:{
+		attack: 5,
+		defense: 4,
+		speed: 4
+	}
+};
+var carta35= {
+	nome: "Nidoking",
+	imagem: "https://ovicio.com.br/wp-content/uploads/nidoking.jpg",
+	atributos:{
+		attack: 6,
+		defense: 5,
+		speed: 5
+	}
+};
+var carta36= {
+	nome: "Clefairy",
+	imagem: "https://media.pocketmonsters.net/characters/1586.png/t/400.jpg",
+	atributos:{
+		attack: 3,
+		defense: 3,
+		speed: 3
+	}
+};
+var carta37= {
+	nome: "Clefable",
+	imagem: "https://i.pinimg.com/originals/e2/a0/f1/e2a0f1ee6120e75b2337677fefef6461.jpg",
+	atributos:{
+		attack: 5,
+		defense: 5,
+		speed: 4
+	}
+};
+var carta38= {
+	nome: "Vulpix",
+	imagem: "https://i.ytimg.com/vi/6YEiLoFwlEc/hqdefault.jpg",
+	atributos:{
+		attack: 3,
+		defense: 3,
+		speed: 4 
+	}
+};
+var carta39= {
+	nome: "Ninetales",
+	imagem: "https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2020/04/legiao_ECrZH5blBY1O.jpg.jpeg",
+	atributos:{
+		attack: 5,
+		defense: 5,
+		speed: 6
+	}
+};
+var carta40= {
+	nome: "Jigglypuff",
+	imagem: "https://sm.ign.com/t/ign_br/screenshot/default/jigglypuff_mv82.h720.jpg",
+	atributos:{
+		attack: 3,
+		defense: 2,
+		speed: 2
+	}
+};
+var carta41= {
+	nome: "Wigglytuff",
+	imagem: "http://3.bp.blogspot.com/-Cfb0XMCWiP8/UIMAJPsQQ8I/AAAAAAAAhYc/yCMC_9Cf_6g/s1600/Wigglytuff_Leer.jpg",
+	atributos:{
+		attack: 5,
+		defense: 3,
+		speed: 3
+	}
+};
+var carta42= {
+	nome: "Zubat",
+	imagem: "http://4.bp.blogspot.com/-un__1h0vv2k/UIcDih9T70I/AAAAAAAAhnQ/sNc2O42cU38/s1600/Zubat_Supersonic.jpg",
+	atributos:{
+		attack: 3,
+		defense: 3,
+		speed: 4
+	}
+};
+var carta43= {
+	nome: "Golbat",
+	imagem: "https://i.redd.it/pvqjmgwdlzr31.jpg",
+	atributos:{
+		attack: 5,
+		defense: 5,
+		speed: 4
+	}
+};
+
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15,
+carta16, carta17, carta18, carta19, carta20, carta21, carta22, carta23, carta24, carta25, carta26, carta27, carta28, carta29, carta30, carta31, carta32, carta33, carta34, carta35,
+carta36, carta37, carta38, carta39, carta40, carta41, carta42, carta43];
+
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta(){
+
+	var numeroCartaMaquina = parseInt(Math.random() * 43);
+	cartaMaquina = cartas[numeroCartaMaquina];
+
+	var numeroCartaJogador = parseInt(Math.random() * 43);
+
+	while(numeroCartaMaquina == numeroCartaJogador){
+		numeroCartaJogador = parseInt(Math.random() * 43);
+	}
+
+	cartaJogador = cartas[numeroCartaJogador];
+
+	document.getElementById('btnSortear').disabled = true;
+	document.getElementById('btnJogar').disabled = false;
+
+	exibirCartaJogador();
+}
+function obtemAtributoSelecionado(){
+
+	var radioAtributos = document.getElementsByName("atributos")
+
+	for(var i = 0; i < radioAtributos.length; i++){
+		if(radioAtributos[i].checked == true){
+			return radioAtributos[i].value
+		}
+	}
+}
+function jogar(){
+	var atributoSelecionado = obtemAtributoSelecionado();
+	var elementoResultado = document.getElementById("resultado");
+
+	var valorCartaJogador = (cartaJogador.atributos[atributoSelecionado]);
+	var valorCartaMaquina = (cartaMaquina.atributos[atributoSelecionado]);
+
+	if (valorCartaJogador > valorCartaMaquina){
+		elementoResultado.innerHTML = "YOU WIN!"
+	} else if (valorCartaJogador < valorCartaMaquina){
+		elementoResultado.innerHTML = "YOU LOOSE :("
+	} else {
+		elementoResultado.innerHTML = "We have a tie..."
+	};
+
+	exibirCartaMaquina();
+}
+function exibirCartaJogador(){
+	var divCartaJogador = document.getElementById("carta-jogador");
+	divCartaJogador.style.backgroundImage=`url(${cartaJogador.imagem})`;
+
+	var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+	var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+	var opcoesTexto = "";
+	for(var atributos in cartaJogador.atributos){
+		opcoesTexto += "<input type='radio' name='atributos' required value='" +atributos + "'>" + atributos + " " + cartaJogador.atributos[atributos] + "<br>";
+	}
+
+	var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>"`;
+
+	divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+function exibirCartaMaquina(){
+	var divCartaMaquina = document.getElementById("carta-maquina");
+	divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.imagem})`;
+
+	var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+	var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+	var opcoesTexto = "";
+	for(var atributos in cartaMaquina.atributos){
+		opcoesTexto += "<p name='atributos' value='" +atributos + "'>" + atributos + " " + cartaMaquina.atributos[atributos] + "</p>";
+	}
+
+	var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>"`;
+
+	divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
